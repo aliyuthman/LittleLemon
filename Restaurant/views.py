9c2c51a932fec02f6be2b6c25b34503aa7b6f87b
Aliyu Usman <aliyuusman64@gmail.com>
2023-08-01 18:02:25 +0100

    static files added and index view implemented

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def sayHello(request):
+    return HttpResponse("Hello")
+
+
+def index(request):
+    return render(request, "index.html", {})
